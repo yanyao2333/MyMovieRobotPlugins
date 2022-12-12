b6266c001133165c8406fa26e94f7756a3a500b4
yanyao <zdc666nb@gmail.com>
2022-12-12 19:39:03 +0800

    fix:使用公开图床上传 feat:为检查cookie有效性功能做准备

--- BilibiliDownloadToEmby/bilibili_login.py
+++ BilibiliDownloadToEmby/bilibili_login.py
@@ -1,8 +1,11 @@
 # -*- coding:utf-8 -*-
 """改造bilibili_api的登录模块，使其能够在微信推送中显示二维码"""
 import json
+import os
+import sys
 import time
 import uuid
+import loguru
 
 import PIL
 import httpx
@@ -15,9 +18,12 @@ from moviebotapi import MovieBotServer
 from moviebotapi.core.session import AccessKeySession
 
 from constant import SERVER_URL, ACCESS_KEY
+import global_value
+import bilibili_main
 
 server = MovieBotServer(AccessKeySession(SERVER_URL, ACCESS_KEY))
 API = get_api("login")
+_LOGGER = loguru.logger
 login_key = ""
 
 
@@ -35,17 +41,22 @@ def pad_image(image, target_size):
 
 
 @tenacity.retry(stop=tenacity.stop_after_attempt(5), wait=tenacity.wait_fixed(5))
-def send_qrcode(token, img):
-    """发送二维码(走mr渠道)"""
-    params = {'token': token}
-    files = {'file': open(img, 'rb')}
-    res = requests.post(url="https://tucang.cc/api/v1/upload", params=params,
-                        files=files)  # 上传二维码用于发送 我是在找不到什么免登录还能有api的图床了。。。就先用这个吧，记得在下面填上token
+def send_qrcode(img):
+    """发送二维码(走mr推送渠道)"""
+    files = {"file": ("qrcode.png", open(img, "rb"), "image/png")}
+    data = {"file_id": 0, "img_type": "catBox"}
+    res = requests.post(
+        url="https://free-pic.hzz.cool/upload.php", files=files, data=data
+    )  # 随便找了个公共图床
     res = res.json()
-    if res['code'] == '200':
-        server.notify.send_message_by_tmpl(title="截图扫码登录（请在120s内登录）",
-                                           context={'pic_url': res['data']['url']},
-                                           body="截图到bilibili扫码登录")
+    if res["code"] == 200:
+        url = res["data"]["url"].replace("\/", "/")
+        server.notify.send_message_by_tmpl(
+            title="截图扫码登录（请在120s内登录）",
+            context={"pic_url": url},
+            body="截图到bilibili扫码登录",
+            to_uid=1,
+        )
         return
     else:
         raise Exception("上传图片失败，重试中")
@@ -62,8 +73,9 @@ def events():
             cookies={"buvid3": str(uuid.uuid1()), "Domain": ".bilibili.com"},
         ).text
     )
-    # if "code" in events.keys() and events["code"] == -412:
-    #     print('nnnnnn')
+    if "code" in events.keys() and events["code"] == -412:
+        _LOGGER.info(events["message"] + "60s后重试")
+        raise exceptions.LoginError(events["message"])
     if isinstance(events["data"], dict):
         url = events["data"]["url"]
         cookies_list = url.split("?")[1].split("&")
@@ -77,9 +89,14 @@ def events():
                 bili_jct = cookie[9:]
             if cookie[:11].upper() == "DEDEUSERID=":
                 dede = cookie[11:]
-        print(f"SESSDATA={sessdata};bili_jct={bili_jct};DEDEUSERID={dede}")
+        if os.path.exists(f"{bilibili_main.local_path}/cookies.txt"):
+            os.remove(f"{bilibili_main.local_path}/cookies.txt")
+        with open(f"{bilibili_main.local_path}/cookies.txt", "w") as f:
+            f.write(json.dumps({"SESSDATA": sessdata, "bili_jct": bili_jct, "DEDEUSERID": dede}))
         c = Credential(sessdata, bili_jct, dedeuserid=dede)
         credential = c
+        global_value.set_value("credential", credential)
+        global_value.set_value("cookie_is_valid", True)
         return credential
 
 
@@ -98,23 +115,23 @@ class LoginBilibili:
     """登录类"""
 
     @tenacity.retry(
-        wait=tenacity.wait_fixed(1), retry=tenacity.retry_if_exception_type(Exception)
+        wait=tenacity.wait_fixed(60), retry=tenacity.retry_if_exception_type(exceptions.LoginError)
     )
     def by_scan_qrcode(self):
         """扫码登录 如果没登录就无限重发"""
-        token = "1670337991933e58927c9c14840038764e8858db545e4"
         img = update_qrcode()
         image = PIL.Image.open(img)
         image = pad_image(image, (300, 100))
         image.save(img)
-        send_qrcode(token, img)
+        send_qrcode(img)
         start = time.time()
         while True:
             credential = events()
             if credential:
-                return credential
+                global_value.set_value("credential", credential)
             else:
                 if time.time() - start > 120:
+                    _LOGGER.error("登录超时")
                     raise exceptions.LoginError("登录超时 60s后再次发送二维码")
 
     def by_cookie(self, SESSDATA, BILI_JCT, BUVID3):
@@ -123,5 +140,8 @@ class LoginBilibili:
         return credential
 
 
-if __name__ == '__main__':
-    print(LoginBilibili().by_scan_qrcode())
+if __name__ == "__main__":
+    login = LoginBilibili()
+    credential = login.by_scan_qrcode()
+    print(credential)
+    print(sync(Credential.check_valid(credential)))
